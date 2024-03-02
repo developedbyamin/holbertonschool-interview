e0a3e3b9c41030015e10cd7639655eaaa69e660d
codelych <aqayevamin095@gmail.com>
2024-03-02 18:52:54 +0400

    first commit

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,15 +4,17 @@ def canUnlockAll(boxes):
         return False
 
     n = len(boxes)
-    unlocked_boxes = set([0])
-    keys_to_process = set([0])  # Initialize with the first box
+    unlocked_boxes = [False] * n
+    unlocked_boxes[0] = True
+    keys = set(boxes[0])
 
-    while keys_to_process:
-        current_box = keys_to_process.pop()
-
+    stack = [0]
+    while stack:
+        current_box = stack.pop()
         for key in boxes[current_box]:
-            if key < n and key not in unlocked_boxes:
-                unlocked_boxes.add(key)
-                keys_to_process.add(key)
+            if key < n and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                keys.update(boxes[key])
+                stack.append(key)
 
-    return len(unlocked_boxes) == n
+    return all(unlocked_boxes)

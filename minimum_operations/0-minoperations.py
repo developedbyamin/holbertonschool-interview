c7f098fb617a425509ab244ff3bec717f8e94c55
developedbyamin <developedbyamin@gmail.com>
2024-03-27 17:50:02 +0400

    minoperations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -21,3 +21,8 @@ def minOperations(n):
     return n
     
 
+n = 4
+print("Min of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min of operations to reach {} char: {}".format(n, minOperations(n)))

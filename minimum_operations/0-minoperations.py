d7dab43af0dc4da07bf286fd6cd0b824e6779719
developedbyamin <developedbyamin@gmail.com>
2024-03-27 18:00:49 +0400

    minoperations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -7,7 +7,7 @@ Minimum number of operations to obtain the desired count of elements.
 
 def minOperations(n):
     if n <= 1:
-        return 0    
+        return 0
     def factors(num):
         res = []
         for i in range(1, int(num**0.5) + 1):
@@ -16,10 +16,9 @@ def minOperations(n):
                 if i != num // i:
                     res.append(num // i)
         return res
-        
+
     for i in factors(n)[1:]:
         if i < n:
             return i + minOperations(n // i)
-    
+
     return n
- 

c53cdd2638d3b2e67838fe8317ba41d2f168bfb8
developedbyamin <developedbyamin@gmail.com>
2024-03-27 17:45:48 +0400

    minoperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    
+    def factors(num):
+        res = []
+        for i in range(1, int(num**0.5) + 1):
+            if num % i == 0:
+                res.append(i)
+                if i != num // i:
+                    res.append(num // i)
+        return res
+
+    for i in factors(n)[1:]:
+        if i < n:
+            return i + minOperations(n // i)
+    
+    return n
+    
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))

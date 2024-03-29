dc9e98c8c2108991d3485d7f2acadd19f5a5fa03
developedbyamin <developedbyamin@gmail.com>
2024-03-29 10:33:39 +0400

    sandpiles

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -3,10 +3,9 @@
 
 #include <stddef.h>
 
-
 static void print_grid(int grid[3][3]);
-
 
 int stable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
 #endif /* _SANDPILES_H_ */

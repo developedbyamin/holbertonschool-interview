6b372687bc0cfcd1fef0a4b9184bf178746ea825
developedbyamin <developedbyamin@gmail.com>
2024-03-29 10:24:52 +0400

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -58,3 +58,30 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
     if (stable(grid1))
         return;
+
+    do
+    {
+        printf("=\n");
+        print_grid(grid1);
+        for (i = 0; i < 3; i++)
+            for (j = 0; j < 3; j++)
+                if (grid1[i][j] > 3)
+                {
+                    grid2[i][j] -= 4;
+                    if (i > 0)
+                        grid2[i - 1][j] += 1;
+                    if (i < 2)
+                        grid2[i + 1][j] += 1;
+                    if (j > 0)
+                        grid2[i][j - 1] += 1;
+                    if (j < 2)
+                        grid2[i][j + 1] += 1;
+                }
+        for (i = 0; i < 3; i++)
+            for (j = 0; j < 3; j++)
+            {
+                grid1[i][j] += grid2[i][j];
+                grid2[i][j] = 0;
+            }
+    } while (!stable(grid1));
+}

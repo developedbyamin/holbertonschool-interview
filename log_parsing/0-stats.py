dad8223b6b75e572e63a7d6180f0b2caed68b34d
developedbyamin <developedbyamin@gmail.com>
2024-04-16 15:55:42 +0400

    initial

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Reads stdin line by line and computes metrics
+If count of lines is evenly divided by 10 and/or
+keyboardinterrupt
+all info will be printed"""
 
 import sys
 import datetime
